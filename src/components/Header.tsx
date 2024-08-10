c1712aa1589103ff11348aa9ed30f6ed22cee666
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-11 00:18:39 +0100

    Added alpha warning

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,6 +27,14 @@ const Header = () => {
           </div>
         </div>
       </div>
+      <div id="header-alpha-warning">
+        <h2>This is an alpha version.</h2>
+        <ul>
+          <li>Some features may be removed or changed without notice.</li>
+          <li>Certain functionalities might not work as expected.</li>
+          <li>We appreciate your patience as we continue to update and test.</li>
+        </ul>
+      </div>
       <div id="header-down">
         <svg className="size-6 h-6 alice-blue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
           <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
