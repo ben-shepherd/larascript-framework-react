8c3c7c5cd9382ffb6b1232d7db7df1e2dfb8c503
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-14 23:19:28 +0100

    Updated console section with collection argument

--- src/components/sections/Documentation/components/docs/Console/ConsoleMakerCommands.tsx
+++ src/components/sections/Documentation/components/docs/Console/ConsoleMakerCommands.tsx
@@ -2,56 +2,56 @@
 const ConsoleAvailableCommands = () => {
     return (
         <article>
-            <p>Creates a new Model file in <code>@src/app/models/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Model</strong> file in <code>@src/app/models/{'{fileName}'}</code></p>
             <pre>
                 <code>
-                    yarn run dev -- make:model --name=MovieModel
+                    yarn run dev -- make:model --name=MovieModel --collection=movies
                 </code>
             </pre>
 
-            <p>Creates a new Repository file in <code>@src/app/repositories/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Repository</strong> file in <code>@src/app/repositories/{'{fileName}'}</code></p>
             <pre>
                 <code>
-                    yarn run dev -- make:command --name=MovieRepository
+                    yarn run dev -- make:command --name=MovieRepository --collection=movies
                 </code>
             </pre>
 
-            <p>Creates a new Service file in <code>@src/app/services/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Service</strong> file in <code>@src/app/services/{'{fileName}'}</code></p>
             <pre>
                 <code>
                     yarn run dev -- make:command --name=MovieService
                 </code>
             </pre>
 
-            <p>Creates a new Singleton Service file in <code>@src/app/services/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Singleton</strong> Service file in <code>@src/app/services/{'{fileName}'}</code></p>
             <pre>
                 <code>
                     yarn run dev -- make:command --name=MovieSingleton
                 </code>
             </pre>
 
-            <p>Creates a new Provider file in <code>@src/app/providers/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Provider</strong> file in <code>@src/app/providers/{'{fileName}'}</code></p>
             <pre>
                 <code>
                     yarn run dev -- make:command --name=MovieProvider
                 </code>
             </pre>
 
-            <p>Creates a new Listener file in <code>@src/app/events/listeners/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Listener</strong> file in <code>@src/app/events/listeners/{'{fileName}'}</code></p>
             <pre>
                 <code>
                     yarn run dev -- make:command --name=MovieListener
                 </code>
             </pre>
 
-            <p>Creates a new Subscriber file in <code>@src/app/events/subscribers/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Subscriber</strong> file in <code>@src/app/events/subscribers/{'{fileName}'}</code></p>
             <pre>
                 <code>
                     yarn run dev -- make:command --name=MovieSubscriber
                 </code>
             </pre>
 
-            <p>Creates a new Observer file in <code>@src/app/events/observers/{'{fileName}'}</code></p>
+            <p>Creates a new <strong>Observer</strong> file in <code>@src/app/events/observers/{'{fileName}'}</code></p>
             <pre>
                 <code>
                     yarn run dev -- make:command --name=MovieObserver
