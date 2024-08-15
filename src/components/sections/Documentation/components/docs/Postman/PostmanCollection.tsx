589a9ab7446e3dca664b447ea4500f917cdc511e
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-08-15 23:50:48 +0100

    Updated postman documentation

--- src/components/sections/Documentation/components/docs/Postman/PostmanCollection.tsx
+++ src/components/sections/Documentation/components/docs/Postman/PostmanCollection.tsx
@@ -9,17 +9,18 @@ const PostmanCollection = () => {
                 <strong>Download here:<br /> <a href="/data/LarascriptNode.postman_collection.json" download target="_blank" rel="noopener noreferrer">LarascriptNode.postman_collection.json</a></strong>
             </div>
 
+            <p>This preconfigured collection saves you time in setting up requests and allows you to quickly test and interact with the authentication system.</p>
+
             <h2>Using the Postman Collection</h2>
             <p>To use the provided Postman collection:</p>
             <ul className="list-disc my-5">
                 <li>Import the collection into your Postman application</li>
-                <li>Set up your environment variables (such as base URL)</li>
-                <li>You can now start testing the API endpoints immediately</li>
+                <li>Update variables in your Postman collection</li>
             </ul>
+            <img src="/images/postman/apiVariables.png" alt="postman api variables" />
+            <p className="mt-3">You can now start testing the API endpoints immediately.</p>
 
-            <p>This preconfigured collection saves you time in setting up requests and allows you to quickly test and interact with the authentication system.</p>
-
-            <h2>Customization</h2>
+            <h2 className="mt-5">Customization</h2>
             <p>While the basic endpoints are provided, you can easily extend or modify the collection to suit your specific needs. Feel free to add new requests, modify existing ones, or create new folders to organize your API testing workflow.</p>
 
             <p><strong>Note:</strong> Remember to update the collection whenever significant changes are made to your API to ensure it remains in sync with your current implementation.</p>
