d6c4e782d01af197fb402344a4a38215607be9b3
BENJAMIN\bensh <ben.shepherd@gmx.com>
2024-09-13 02:08:24 +0100

    Updated about section

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -2,15 +2,14 @@ const About = () => {
     return (
         <div id="section-about" className='section py-24'>
             <h1 className='text-center mb-10'>Meet The Author</h1>
-            <div className="container">
-                <div className='flex flex-wrap lg:flex-nowrap'>
-                    <div className='section_about__author_image_container w-full lg:w-1/2' title="Handsome picture">
-                            <div className='section_about__author_image mr-24'></div>
-                            <div className='section_about__author_image_goofy mr-24'></div>
+                <div className='w-full lg:w-1/2 flex flex-wrap lg:flex-nowrap justify-center'>
+                    <div className='section_about__author_image_container w-full md:w-1/2' title="Handsome picture">
+                            <div className='section_about__author_image'></div>
+                            <div className='section_about__author_image_goofy'></div>
                     </div>
                     <div className="info-container w-full lg:w-1/2">
-                        <h3 className='author-text text-2xl'>Benjamin Shepherd</h3>
-                        <p className='role text-xs tex'>Software Developer</p>
+                        <h3 className='author-text text-2xl mb-3'>Benjamin S</h3>
+                        <p className='role text-xs tex'>Full Stack / Software Developer</p>
 
                         <p className='mt-3 bio'>
                             Hello, I'm Benjamin, an experienced software developer with expertise in Node.js, Typescript, MongoDB, Express, PHP, Laravel, Symfony, SQL/Postgres, and React. My passion for technology drives me to constantly innovate and improve my skills. I believe in the power of code to transform ideas into reality and am committed to developing top-notch web applications. Feel free to connect with me to discuss projects, technology, or just to share ideas.
@@ -28,11 +27,14 @@ const About = () => {
                             <strong>LinkedIn</strong>:<br/> <a href='https://www.linkedin.com/in/benjamin-programmer/'
                                                                target='_blank'>https://www.linkedin.com/in/benjamin-programmer/</a>
                         </p>
+
+                        <p className='mt-3 discord'>
+                            <strong>Discord</strong>:<br/> <a href='javascript: void(0);'>benjaminjs94</a>
+                        </p>
                     </div>
                 </div>
-            </div>  
         </div>
-    )
+    )   
 }
 
 export default About
